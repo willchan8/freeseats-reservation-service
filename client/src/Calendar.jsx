d4c720eada06b89f560ccda4c6fa487e960651e8
Winnie Fong <winniefong21@gmail.com>
2019-05-17 20:01:17 -0700

    Able to render current month on app

--- client/src/Calendar.jsx
+++ client/src/Calendar.jsx
@@ -9,18 +9,76 @@ class Calendar extends React.Component {
 
     this.state = {
       weekDays: moment.weekdaysShort(),
+      firstDayOfMonth: moment(),
+      daysInMonth: moment(),
     };
   }
 
+  firstDayOfMonth() {
+    const dateObj = this.state.firstDayOfMonth;
+    const firstDay = moment(dateObj).startOf('month').format('d');
+    return firstDay;
+  }
+
+  daysInMonth() {
+    const dateObj = this.state.daysInMonth;
+    const monthDays = moment(dateObj).daysInMonth();
+    // may need to change to find # of days in current month
+    // moment("2012-01", "YYYY-MM").daysInMonth() // 31
+    return monthDays;
+  }
+
   render() {
+    const blanks = [];
+    for (let i = 0; i < this.firstDayOfMonth(); i++) {
+      blanks.push(
+        <td key={i} className="calendar-day-res empty">{''}</td>,
+      );
+    }
+
+    const daysInMonth = [];
+    for (let d = 1; d <= this.daysInMonth(); d++) {
+      daysInMonth.push(
+        <td key={d} className="calendar-day-res">
+          {d}
+        </td>,
+      );
+    }
+
+    const totalSlots = [...blanks, ...daysInMonth];
+    const rows = [];
+    let cells = [];
+
+    totalSlots.forEach((row, i) => {
+      if (i % 7 !== 0) {
+        cells.push(row);
+      } else {
+        rows.push(cells);
+        cells = [];
+        cells.push(row);
+      }
+      if (i === totalSlots.length - 1) {
+        rows.push(cells);
+      }
+    });
+
     return (
-      <div>
-        {this.state.weekDays.map((day) => {
-          return <th key={day} className="week-day">
-            {day}
-          </th>;
-        })}
-      </div>
+      <table className="reservations-calendar">
+        <thead>
+          <tr>
+            {this.state.weekDays.map((day) => {
+              return <th key={day} className="week-day-res">
+                {day}
+              </th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((d, i) => {
+            return <tr key={i} >{d}</tr>;
+          })}
+        </tbody>
+      </table>
     );
   }
 }
