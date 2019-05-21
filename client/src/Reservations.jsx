8705b85e135902693a1ba30afb34fca04f9e79c1
Winnie Fong <winniefong21@gmail.com>
2019-05-20 21:15:57 -0700

    Add border to calendar component

--- client/src/Reservations.jsx
+++ client/src/Reservations.jsx
@@ -2,26 +2,31 @@ import React from 'react';
 
 import Calendar from './Calendar.jsx';
 
-// class Reservations extends React.Component {
-//   constructor(props) {
-//     super(props);
+class Reservations extends React.Component {
+  constructor(props) {
+    super(props);
 
-//     this.state = {};
-//   }
+    this.state = {};
+  }
 
-//   render() {
-//     return(
-//       <div>Hello World</div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <div className="reservation-title-wrapper">
+          <span className=""></span>
+        </div>
+        <Calendar />
+      </div>
+    );
+  }
+}
 
-const Reservations = () => {
-  return (
-    <div>
-      <Calendar />
-    </div>
-  );
-};
+// const Reservations = () => {
+//   return (
+//     <div>
+//       <Calendar />
+//     </div>
+//   );
+// };
 
 export default Reservations;
