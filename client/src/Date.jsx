fca66423a41876be1f4b02673c80b5d4ade2c038
Winnie Fong <winniefong21@gmail.com>
2019-05-22 20:11:39 -0700

    Refactor date and calendar to appear on app correctly

--- client/src/Date.jsx
+++ client/src/Date.jsx
@@ -1,63 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import dateFns from 'date-fns';
-import moment from 'moment';
 
-import Calendar from './Calendar.jsx';
-
-class Date extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      date: 'Today',
-      displayCalendar: false,
-    };
-
-    this.handleClickCalendar = this.handleClickCalendar.bind(this);
-    this.hideCalendar = this.hideCalendar.bind(this);
-    this.changeDate = this.changeDate.bind(this);
-  }
-
-  handleClickCalendar() {
-    this.setState({
-      displayCalendar: true,
-    });
-  }
-
-  hideCalendar() {
-    this.setState({
-      displayCalendar: false,
-    });
-  }
-
-  changeDate(e) {
-    if (dateFns.format(e, 'ddd, M/D') !== dateFns.format(moment(), 'ddd, M/D')) {
-      this.setState({
-        date: dateFns.format(e, 'ddd, M/D'),
-      });
-    } else if (dateFns.format(e, 'ddd, M/D') === dateFns.format(moment(), 'ddd, M/D')) {
-      this.setState({
-        date: 'Today',
-      });
-    }
-  }
-
-  render() {
-    return (
-      <div className="date-wrapper">
-        <div className="date-title">Date</div>
-        <div className="display-date" onClick={this.handleClickCalendar} >
-          {this.state.date}
-        </div>
-        {this.state.displayCalendar ? <Calendar changeDate={this.changeDate} handleDate={this.props.handleDate} hideCalendar={this.hideCalendar} /> : null}
+const Date = (props) => {
+  return (
+    <div className="date-wrapper">
+      <div className="date-title">Date</div>
+      <div className="display-date" onClick={props.handleClick} >
+        {props.clickedDate}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Date.propTypes = {
-  handleDate: PropTypes.func,
+  handleClick: PropTypes.func,
+  clickedDate: PropTypes.string,
 };
 
 export default Date;
--- client/src/Reservations.jsx
+++ client/src/Reservations.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import moment from 'moment';
+import dateFns from 'date-fns';
 
 import PartySize from './PartySize.jsx';
 import Time from './Time.jsx';
 import Date from './Date.jsx';
+import Calendar from './Calendar.jsx';
 
 import '../../public/styles.css';
 
@@ -14,12 +16,17 @@ class Reservations extends React.Component {
     this.state = {
       partySize: 2,
       date: moment(),
+      clickedDate: 'Today',
       time: '6:00 PM',
+      displayCalendar: false,
     };
 
     this.handleSize = this.handleSize.bind(this);
     this.handleTime = this.handleTime.bind(this);
     this.handleDate = this.handleDate.bind(this);
+    this.handleClickCalendar = this.handleClickCalendar.bind(this);
+    this.hideCalendar = this.hideCalendar.bind(this);
+    this.changeDate = this.changeDate.bind(this);
   }
 
   handleSize(e) {
@@ -40,6 +47,30 @@ class Reservations extends React.Component {
     });
   }
 
+  handleClickCalendar() {
+    this.setState({
+      displayCalendar: true,
+    });
+  }
+
+  hideCalendar() {
+    this.setState({
+      displayCalendar: false,
+    });
+  }
+
+  changeDate(e) {
+    if (dateFns.format(e, 'ddd, M/D') === dateFns.format(moment(), 'ddd, M/D')) {
+      this.setState({
+        clickedDate: 'Today',
+      });
+    } else {
+      this.setState({
+        clickedDate: dateFns.format(e, 'ddd, M/D'),
+      });
+    }
+  }
+
   render() {
     return (
       <div className="reservations">
@@ -48,9 +79,10 @@ class Reservations extends React.Component {
         </div>
         <PartySize handleSize={this.handleSize} />
         <div className="date-time-wrapper">
-          <Date handleDate={this.handleDate} />
+          <Date handleClick={this.handleClickCalendar} clickedDate={this.state.clickedDate} />
           <Time handleTime={this.handleTime} />
         </div>
+        {this.state.displayCalendar ? <Calendar hideCalendar={this.hideCalendar} handleDate={this.handleDate} changeDate={this.changeDate} /> : null}
       </div>
     );
   }
