8271d6a3df0c3454c0675f30e877b74a1911fb5f
Winnie Fong <winniefong21@gmail.com>
2019-05-25 21:51:49 -0700

    Handle edge case of no time slots available

--- client/src/Reservations.jsx
+++ client/src/Reservations.jsx
@@ -245,7 +245,7 @@ class Reservations extends React.Component {
 
         {this.state.findTableBtn ? <button className="find-table" onClick={this.checkAvailability}>Find a Table</button> : null}
 
-        {this.state.availTimes ? <AvailTimes availTimeSlots={this.state.availTimeSlots} time={this.state.time} selectedDate={this.state.date} /> : null}
+        {this.state.availTimes ? <AvailTimes availTimeSlots={this.state.availTimeSlots} time={this.state.time} selectedDate={this.state.date} noAvailability={this.noAvailability} /> : null}
 
         {this.state.noAvailMsg ? <NoAvailability time={this.state.time} /> : null}
 
--- client/src/conditional_messages/AvailTimes.jsx
+++ client/src/conditional_messages/AvailTimes.jsx
@@ -4,6 +4,7 @@ import dateFns from 'date-fns';
 
 import '../../../public/styles.css';
 
+// refactor: lots of repetitive code
 const AvailTimes = (props) => {
   const checkTime = props.time.split(' ');
   const checkHour = checkTime[0].split(':');
@@ -11,8 +12,6 @@ const AvailTimes = (props) => {
   const timeSlots = props.availTimeSlots;
   let times;
   const currentHour = Number(dateFns.format(new Date(), 'H'));
-
-  console.log(props.time);
 
   if (dateFns.format(props.selectedDate, 'M/D/YY') === dateFns.format(new Date(), 'M/D/YY')) {
     const todayTimes = [];
@@ -30,75 +29,99 @@ const AvailTimes = (props) => {
         </div>;
       });
     }
-  }
 
-  if (hour === 4) {
-    const timesFor4 = [];
-    for (let i = 0; i < timeSlots.length; i++) {
-      const hour = Number(timeSlots[i].split(' ')[0].split(':')[0]);
-      if (hour === 6) {
-        timesFor4.push(timeSlots[i]);
-      }
+    if (todayTimes.length === 0) {
+      props.noAvailability();
     }
-
-    times = timesFor4.map(time => {
-      return <div className="avail-time" key={time} >
-        <span className="avail-time-caption">{time}</span>
-      </div>;
-    });
-
-  } else if (hour === 5) {
-    const timesFor5 = [];
-
-    for (let i = 0; i < timeSlots.length; i++) {
-      const hour = Number(timeSlots[i].split(' ')[0].split(':')[0]);
-      if (hour === 6 || hour === 7) {
-        timesFor5.push(timeSlots[i]);
-      }
-    }
-
-    times = timesFor5.map(time => {
-      return <div className="avail-time" key={time} >
-        <span className="avail-time-caption">{time}</span>
-      </div>;
-    });
-
-  } else if (hour === 9) {
-    const timesFor9 = [];
-    for (let i = 0; i < timeSlots.length; i++) {
-      const hour = Number(timeSlots[i].split(' ')[0].split(':')[0]);
-      if (hour === 7 || hour === 8) {
-        timesFor9.push(timeSlots[i]);
-      }
-    }
-
-    times = timesFor9.map(time => {
-      return <div className="avail-time" key={time} >
-        <span className="avail-time-caption">{time}</span>
-      </div>;
-    });
-
-  } else if (hour === 10) {
-    const timesFor10 = [];
-    for (let i = 0; i < timeSlots.length; i++) {
-      const hour = Number(timeSlots[i].split(' ')[0].split(':')[0]);
-      if (hour === 8 || timeSlots[i] === '7:30 PM' || timeSlots[i] === '7:45 PM') {
-        timesFor10.push(timeSlots[i]);
-      }
-    }
-
-    times = timesFor10.map(time => {
-      return <div className="avail-time" key={time} >
-        <span className="avail-time-caption">{time}</span>
-      </div>;
-    });
-
   } else {
-    times = props.availTimeSlots.map(time => {
-      return <div className="avail-time" key={time} >
-        <span className="avail-time-caption">{time}</span>
-      </div>;
-    });
+    if (hour === 4) {
+      const timesFor4 = [];
+      for (let i = 0; i < timeSlots.length; i++) {
+        const hour = Number(timeSlots[i].split(' ')[0].split(':')[0]);
+        if (hour === 6) {
+          timesFor4.push(timeSlots[i]);
+        }
+      }
+
+      times = timesFor4.map(time => {
+        return <div className="avail-time" key={time} >
+          <span className="avail-time-caption">{time}</span>
+        </div>;
+      });
+
+      if (times.length === 0) {
+        props.noAvailability();
+      }
+
+    } else if (hour === 5) {
+      const timesFor5 = [];
+
+      for (let i = 0; i < timeSlots.length; i++) {
+        const hour = Number(timeSlots[i].split(' ')[0].split(':')[0]);
+        if (hour === 6 || hour === 7) {
+          timesFor5.push(timeSlots[i]);
+        }
+      }
+
+      times = timesFor5.map(time => {
+        return <div className="avail-time" key={time} >
+          <span className="avail-time-caption">{time}</span>
+        </div>;
+      });
+
+      if (times.length === 0) {
+        props.noAvailability();
+      }
+
+    } else if (hour === 9) {
+      const timesFor9 = [];
+      for (let i = 0; i < timeSlots.length; i++) {
+        const hour = Number(timeSlots[i].split(' ')[0].split(':')[0]);
+        if (hour === 7 || hour === 8) {
+          timesFor9.push(timeSlots[i]);
+        }
+      }
+
+      times = timesFor9.map(time => {
+        return <div className="avail-time" key={time} >
+          <span className="avail-time-caption">{time}</span>
+        </div>;
+      });
+
+      if (times.length === 0) {
+        props.noAvailability();
+      }
+
+    } else if (hour === 10) {
+      const timesFor10 = [];
+      for (let i = 0; i < timeSlots.length; i++) {
+        const hour = Number(timeSlots[i].split(' ')[0].split(':')[0]);
+        if (hour === 8 || timeSlots[i] === '7:30 PM' || timeSlots[i] === '7:45 PM') {
+          timesFor10.push(timeSlots[i]);
+        }
+      }
+
+      times = timesFor10.map(time => {
+        return <div className="avail-time" key={time} >
+          <span className="avail-time-caption">{time}</span>
+        </div>;
+      });
+
+      if (times.length === 0) {
+        props.noAvailability();
+      }
+
+    } else {
+      times = props.availTimeSlots.map(time => {
+        return <div className="avail-time" key={time} >
+          <span className="avail-time-caption">{time}</span>
+        </div>;
+      });
+
+      if (times.length === 0) {
+        props.noAvailability();
+      }
+    }
   }
 
   return (
@@ -119,6 +142,7 @@ AvailTimes.propTypes = {
   availTimeSlots: PropTypes.array,
   time: PropTypes.string,
   selectedDate: PropTypes.any.isRequired,
+  noAvailability: PropTypes.func,
 };
 
 export default AvailTimes;
